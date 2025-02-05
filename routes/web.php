06236d9b666b715356a08a92cd52fec9c9c5ad63
Mansur <ttytkhkttymi@gmail.com>
2025-02-05 14:11:11 +0500

    third commit

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,13 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'dashboard'])->name('dashboard')->middleware('auth');
 
-Route::get('loginPage', [UserController::class, 'loginPage'])->name('loginPage');
-Route::get('registerPage', [UserController::class, 'registerPage'])->name('registerPage');
-Route::get('logout', [UserController::class, 'logout'])->name('logout');
+Route::middleware('guest')->group(function () {
+    Route::get('loginPage', [UserController::class, 'loginPage'])->name('loginPage');
+    Route::get('registerPage', [UserController::class, 'registerPage'])->name('registerPage');
+    Route::post('login', [UserController::class, 'login'])->name('login');
+    Route::post('register', [UserController::class, 'register'])->name('register');
+});
 Route::get('editProfile', [UserController::class, 'editProfile'])->name('editProfile')->middleware('auth');
-
-Route::post('login', [UserController::class, 'login'])->name('login');
-Route::post('register', [UserController::class, 'register'])->name('register');
 Route::put('updateProfile', [UserController::class, 'updateProfile'])->name('updateProfile')->middleware('auth');
+Route::get('logout', [UserController::class, 'logout'])->name('logout');
+
